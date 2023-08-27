0f8f6d04eea14298487d8cc99aa677d661d8310b
Dillon Wyatt <dostaglou@gmail.com>
2023-08-27 22:37:39 +0900

    updated to have username requirement on db

--- db/schema.rb
+++ db/schema.rb
@@ -61,7 +61,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_24_114834) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.string "username"
+    t.string "username", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
